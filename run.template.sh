f23d8311d8ce8ba91a14e3f2901db91003f6fc8c
Kian Ahrabian <kian.ahrabian@mail.mcgill.ca>
2020-05-05 15:13:52 -0400

    update model save path

--- run.template.sh
+++ run.template.sh
@@ -15,12 +15,13 @@ source ${VENVDIR}/gg/bin/activate
 
 python -u -c 'import torch; print(torch.__version__)'
 
+MODEL=RotatE
 DATASET=GitGraph_TI_0.01
-SAVE=models/${DATASET}_${SLURM_JOB_ID}
+SAVE=models/${MODEL}_${DATASET}_${SLURM_JOB_ID}
 
 python -u codes/run.py \
-    --dataset data/$DATASET \
-    --model RotatE \
+    --dataset data/${DATASET} \
+    --model ${MODEL} \
     --static_dim 128 \
     --absolute_dim 256 \
     --relative_dim 128,128,128 \
@@ -36,7 +37,7 @@ python -u codes/run.py \
     --test_batch_size 1 \
     --max_steps 200000 \
     --warm_up_steps 100000 \
-    --save_path $SAVE \
+    --save_path ${SAVE} \
     --mode head \
     --valid_steps 40000 \
     --log_steps 100 \
